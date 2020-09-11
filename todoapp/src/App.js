365b9b4bf3245813ff9925f829d92d4663f6d013
“Umair <ubsheikh.4@gmail.com>
2020-09-11 22:59:03 +0100

    Todo App - Phase 1

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="checkbox" id="grocery1" name="grocery1" value="Fruits"></input>
+      <label for="grocery1"> Buy fruits</label><br></br>
+      <input type="checkbox" id="grocery2" name="grocery2" value="Cake"></input>
+      <label for="grocery2">Buy cake</label><br></br>
+      <input type="checkbox" id="grocery3" name="grocery3" value="Pepsi"></input>
+      <label for="vehicle3">Buy Pepsi</label><br></br>
     </div>
-  );
+  )
+
 }
 
 export default App;
