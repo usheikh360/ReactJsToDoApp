e2de369fe1371a0349b1d86812984513a750f6eb
“Umair <ubsheikh.4@gmail.com>
2020-09-12 20:14:00 +0100

    Side activity Changing State

--- todoapp/src/components/ToDoItem.js
+++ todoapp/src/components/ToDoItem.js
@@ -1,31 +1,9 @@
 import React from "react"
 
-//below is the first method for adding the props
-// function ToDoItem(props) {
-//     return (
-//         <div className="todo-item">
-//             <input type="checkbox"></input>
-//             <p>{props.item}</p>
-//             {/* <p>Placeholder text here</p> */}
-//         </div>
-//     )
-// }
-
-//below is the second method for adding the props by providing an object
-// function ToDoItem(props) {
-//     return (
-//         <div className="todo-item">
-//             <input type="checkbox"></input>
-//             <p>{props.itemName.item}</p>
-//             {/* <p>Placeholder text here</p> */}
-//         </div>
-//     )
-// }
-
 function ToDoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed}></input>
+            <input type="checkbox" checked={props.item.completed} onChange={console.log("Changed!")}></input>
             <p>{props.item.id} {props.item.text}</p>
             {/* <p>Placeholder text here</p> */}
         </div>
