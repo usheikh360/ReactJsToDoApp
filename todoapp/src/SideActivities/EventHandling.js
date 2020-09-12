2bb46645697dab22cfde637bdf8f84ca292fffb4
“Umair <ubsheikh.4@gmail.com>
2020-09-12 19:29:34 +0100

    Side activity - Handling Events in React

---
+++ todoapp/src/SideActivities/EventHandling.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function handleClick() {
+    console.log("I was clicked")
+}
+
+// https://reactjs.org/docs/events.html#supported-events
+
+function App() {
+    return (
+        <div>
+            <img onMouseOver={() => console.log("Hovered!")} src="https://www.fillmurray.com/200/100" />
+            <br />
+            <br />
+            <button onClick={handleClick}>Click me</button>
+        </div>
+    )
+}
